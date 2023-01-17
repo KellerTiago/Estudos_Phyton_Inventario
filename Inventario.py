cc9e2232fcafc01a4e5e2c89aab33e1be8dd8ef3
KellerTiago <kellertiago@hotmail.com>
2023-01-16 23:07:46 -0300

    Transformando as linhas de condigo em funcoes separadas

--- Inventario.py
+++ Inventario.py
@@ -1,20 +1,13 @@
-from selecionar_opcao import listar_opcoes
+from selecionar_opcao import *
 inventario = {}
 
 opcao = listar_opcoes()
 
 while opcao > 0 and opcao < 4:
     if opcao == 1:
-        resp = "S"
-        while resp == "S":
-            inventario [input("Digite o numero patrimonial: ")] = [input("Digite a ultima data de atualização: "), input("Digite a descrição do bem: "), input("Digite o departamento: ")]
-            resp = input("Digite S para continuar: ").upper()
+        registrar(inventario)
     elif opcao == 2:
-        with open("inventario.csv", "a") as inv:
-            for chave, valor in inventario.items():
-                inv.write(f"{chave};{valor[0]};{valor[1]};{valor[2]};\n")
-            print("Documento salvo com sucesso !")
+        persistir(inventario)
     elif opcao == 3:
-        with open("inventario.csv", "r") as inv:
-            print(inv.readlines())
+        exibir()
     opcao = listar_opcoes()
--- selecionar_opcao.py
+++ selecionar_opcao.py
@@ -1,3 +1,21 @@
 def listar_opcoes ():
     opcao = int(input("Digite:\n (1) para registrar um ativo:\n (2) para salvar em arquivo:\n (3) para exibir os arquivos armazenados:"))
     return opcao
+
+def registrar (inventario):
+    resp = "S"
+    while resp == "S":
+        inventario [input("Digite o numero patrimonial: ")] = [input("Digite a ultima data de atualização: "), input("Digite a descrição do bem: "), input("Digite o departamento: ")]
+        resp = input("Digite S para continuar: ").upper()
+def persistir (inventario):
+    with open("inventario.txt", "a") as inv:
+        for chave, valor in inventario.items():
+            inv.write(f"{chave};{valor[0]};{valor[1]};{valor[2]}\n")
+    return "Salvo com sucesso"
+def exibir ():
+    with open("inventario.txt", "r") as inv:
+        linhas = inv.readlines()
+    return linhas
+
+
+
